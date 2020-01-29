fd4d16a2b9873217ea525769a4c3801ad4320ec3
LazavikouArtsiom <lazovikovartyom@mail.ru>
2020-01-29 21:18:58 +0300

    C

---
+++ Singltone.py
@@ -0,0 +1,32 @@
+class Singleton:
+    existence = None
+
+    def __new__(cls):
+        if cls.existence is None:
+            cls.existence = super(Singleton, cls).__new__(cls)
+        return cls.existence
+
+
+def SingletonDecorator(cls):
+    cash = {}
+
+    def wrapper(*args, **kwargs):
+        if cls not in cash:
+            cash[cls] = cls(*args, **kwargs)
+        return cash[cls]
+
+    return wrapper()
+
+
+@SingletonDecorator
+class Foo: pass
+    
+
+
+if __name__ == '__main__':
+    s = Singleton()
+    print(s)
+    s1 = Singleton()
+    print(s1)
+    a = Foo()
+
