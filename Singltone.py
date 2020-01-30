23034ec1120271c6923664ab22962b272c6745b8
LazavikouArtsiom <lazovikovartyom@mail.ru>
2020-01-30 22:10:17 +0300

    Update Singltone.py

--- Singltone.py
+++ Singltone.py
@@ -1,32 +1,43 @@
-class Singleton:
-    existence = None
+# class Singleton:
+#    existence = None
 
-    def __new__(cls):
-        if cls.existence is None:
-            cls.existence = super(Singleton, cls).__new__(cls)
-        return cls.existence
+#    def __new__(cls):
+#        if cls.existence is None:
+#            cls.existence = super(Singleton, cls).__new__(cls)
+#        return cls.existence
 
 
-def SingletonDecorator(cls):
+# def SingletonDecorator(cls):
+#    cash = {}
+#
+#    def wrapper(*args, **kwargs):
+#        if cls not in cash:
+#           cash[cls] = cls(*args, **kwargs)
+#        return cash[cls]
+
+#    return wrapper
+
+
+def SingletonDecorator(self):
     cash = {}
 
     def wrapper(*args, **kwargs):
-        if cls not in cash:
-            cash[cls] = cls(*args, **kwargs)
-        return cash[cls]
+        if self not in cash:
+            cash[self] = self
+        return cash[self]
 
-    return wrapper()
+    return wrapper
 
 
 @SingletonDecorator
 class Foo: pass
-    
 
 
 if __name__ == '__main__':
-    s = Singleton()
-    print(s)
-    s1 = Singleton()
-    print(s1)
+    #s = Singleton()
+    #print(s)
+    #s1 = Singleton()
+    #print(s1)
     a = Foo()
-
+    b = Foo()
+    print(a is b)
